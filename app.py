fd5a98ef6aeaedc2d000aae993293f53dec6140f
Javier Hernandez <jhernandez@partner.fluidra.com>
2026-08-28 15:13:28 -0500

    APP OK

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from langfuse.langchain import CallbackHandler
 from langchain_core.messages import HumanMessage
 
 # Import your compiled LangGraph workflow
-from src.agent.graph import graph
-from src.ui.copy import (
+from .src.agent.graph import graph
+from .src.ui.copy import (
     FOOTER_PHASE,
     FOOTER_TEAM,
     PAGE_SUBTITLE,
